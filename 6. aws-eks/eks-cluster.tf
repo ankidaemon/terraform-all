fcc1053cfadb08981f2ae324a0e1551fdc2e205f
ankidaemon <ankitmishra1701@gmail.com>
2020-03-15 22:05:10 +0400

    eks cluster,node group

---
+++ 6. aws-eks/eks-cluster.tf
@@ -0,0 +1,60 @@
+resource "aws_iam_role" "eks-cluster-iam" {
+  name = "terraform-eks-cluster-iam"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster-iam.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-cluster-iam.name
+}
+
+resource "aws_security_group" "eks-cluster-sg" {
+  name        = "terraform-eks-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.eks-vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-eks-sg"
+  }
+}
+
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.eks-cluster-iam.arn
+
+  vpc_config {
+    security_group_ids = [aws_security_group.eks-cluster-sg.id]
+    subnet_ids         = aws_subnet.eks-subnet[*].id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy,
+  ]
+}
---
+++ 6. aws-eks/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster-name" {
+  default = "terraform-eks"
+}
+
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {}
